03c34833ca343cf96fceeb063181bd76da9bd1fa
RasmusBroborg <rasmus.broborg@icloud.com>
2022-03-05 15:39:49 +0100

    Add context classes for application.

---
+++ JohanRasmus/JohanRasmus/Data/Entities/UserNote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JohanRasmus.Data.Entities
+{
+	public class UserNote
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public int IdentityId { get; set; }
+
+		[MaxLength(160)]
+		public string Note { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime Created { get; set; } = DateTime.Now;
+	}
+}
